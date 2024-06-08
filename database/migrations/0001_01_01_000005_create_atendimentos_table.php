871fbe6bab926bf3c3bbfea44fd69ee5ff892e2b
Matteus Felippe <teu2804@hotmail.com>
2024-06-08 15:12:31 -0300

    feat: ambiente virtual e tabelas

---
+++ database/migrations/0001_01_01_000005_create_atendimentos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('atendimentos', function (Blueprint $table) {
+            $table->id();
+            $table->date('data_atendimento');
+            
+            $table->foreignId('medico_id')->constrained(
+                table: 'medicos'
+            )->onDelete("cascade");
+            
+            $table->foreignId('paciente_id')->constrained(
+                table: 'pacientes'
+            )->onDelete("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('atendimentos');
+    }
+};
